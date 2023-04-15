0f2e9246823b5a48235eab5e7f57667027ffb30d
Selçuk Batıhan <abathan95@gmail.com>
2023-04-15 20:38:19 +0300

    Update USLUSAYI.java

--- USLUSAYI.java
+++ USLUSAYI.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class USLUSAYI {
     public static void main(String[] args) {
         Scanner inp=new Scanner(System.in);
-        int say1,say2,sonuc=1,i=1;
+        int say1,say2,sonuc=1;
         System.out.println("sayıyı giriniz");
         say1= inp.nextInt();
         System.out.println("üs giriniz");
@@ -12,11 +12,8 @@ public class USLUSAYI {
             System.out.println("Sonuç: 1");
         }
         else {
-            while (i <= say2) {
+            for(int i=1;i<= say2;i++) {
                 sonuc *= say1;
-                i++;
-
-
             }
             System.out.println("Sonuç: " + sonuc);
         }
